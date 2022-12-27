72636eaa09d45a0e3d72770beacfff4474552eb2
Turchynyk Oleksii <105796938+TurchynykOleksii@users.noreply.github.com>
2022-12-27 21:48:40 +0200

    done

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+const API_KEY = '31333113-bd66c453b27bf0e534c1413fe';
+axios.defaults.baseURL = 'https://pixabay.com/api';
+axios.defaults.params = {
+  orientation: 'landscape',
+  per_page: 12,
+  image_type: 'photo',
+};
+export const getImages = async (query, page) => {
+  const { data } = await axios.get(`/?q=${query}&page=${page}&key=${API_KEY}`);
+
+  return data;
+};
